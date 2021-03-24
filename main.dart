a4d0ad08fe4ba0189a9f41caaca90912676614cd
dream10can <dream10candeveloper@gmail.com>
2021-03-24 20:19:02 +0530

    new example of function

--- main.dart
+++ main.dart
@@ -1,5 +1,20 @@
 main() {
-  final sayHi = (String name) => 'Hi $name';
+  // example 1
+  // final sayHi = (String name) => 'Hi $name';
 
-  print(sayHi('ahmed'));
+  // print(sayHi('ahmed'));
+
+// example 2
+
+  final myFunc = goodEvening;
+
+  loop(myFunc, 'nasser');
+}
+
+void goodEvening(String name) => print('Good Evening :$name');
+
+void loop(void Function(String value) funcName, String name) {
+  for (var i = 0; i < 4; i++) {
+    funcName(name);
+  }
 }
